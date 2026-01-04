ed349dce0c3f69fc19b413240d85fc9952ceeaed
Gagandeep Singh <gagan112002@gmail.com>
2026-01-04 17:58:54 +0530

    second commit

--- client/src/component/Signup.jsx
+++ client/src/component/Signup.jsx
@@ -13,7 +13,7 @@ const Signup = () => {
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
+    setFormData({ ...formData, [e.target.name]: e.target.value })
   };
 
   const handleSubmit = async (e) => {
